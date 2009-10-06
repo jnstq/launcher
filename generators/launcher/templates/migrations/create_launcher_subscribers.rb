a89d0a215d41db1fb79f899a47743f3685f672a0
Jon Stenqvist <jon.stenqvist@gmail.com>
2009-10-06 09:36:14 +0200

    Typofix

--- generators/launcher/templates/migrations/create_launcher_subscribers.rb
+++ generators/launcher/templates/migrations/create_launcher_subscribers.rb
@@ -2,7 +2,7 @@ class CreateLauncherSubscribers < ActiveRecord::Migration
   def self.up
     create_table :launcher_subscribers do |t|
       t.string   :email
-      t.timespans
+      t.timestamps
     end
   end
   
